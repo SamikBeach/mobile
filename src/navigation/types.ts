726279ff10b816c11e1a9807708c89a5521b8cce
bong <jungcome7@gmail.com>
2025-02-04 02:42:03 +0900

    refactor: 불필요한 타입 제거

--- src/navigation/types.ts
+++ src/navigation/types.ts
@@ -1,42 +1,3 @@
-import type { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { NavigatorScreenParams } from '@react-navigation/native';
-
-export type HomeStackParamList = {
-  Home: undefined;
-  Review: {
-    reviewId: number;
-  };
-};
-
-export type BookStackParamList = {
-  BookList: undefined;
-  BookDetail: {
-    bookId: number;
-  };
-};
-
-export type AuthorStackParamList = {
-  AuthorList: undefined;
-  AuthorDetail: { authorId: number };
-};
-
-export type AuthStackParamList = {
-  Login: undefined;
-  SignUp: undefined;
-  UserInfo: { email: string };
-  VerifyCode: { email: string };
-  ResetPassword: {
-    email: string;
-    token: string;
-  };
-};
-
-export type UserStackParamList = {
-  User: {
-    userId?: number;
-  };
-};
-
 export type TabParamList = {
   HomeTab: undefined;
   BookTab: undefined;
@@ -59,8 +20,3 @@ export type RootStackParamList = {
   BookDetail: { bookId: number };
   AuthorDetail: { authorId: number };
 };
-
-export type RootStackScreenProps<T extends keyof RootStackParamList> = NativeStackScreenProps<
-  RootStackParamList,
-  T
->;
