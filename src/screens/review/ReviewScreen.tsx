24cc6d698283249879d10992cfafc9f3e19e45af
bong <jungcome7@gmail.com>
2025-02-10 20:42:40 +0900

    fix: 스타일 수정

--- src/screens/review/ReviewScreen.tsx
+++ src/screens/review/ReviewScreen.tsx
@@ -197,7 +197,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'white',
-    paddingTop: 12,
+    paddingTop: 16,
   },
   contentContainer: {
     flex: 1,
