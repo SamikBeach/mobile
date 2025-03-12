55243b2dfce91ab67604aca3510b60ae3ae60240
bong <jungcome7@gmail.com>
2025-03-12 13:40:09 +0900

    chore: 주석 추가

--- src/apis/genre.ts
+++ src/apis/genre.ts
@@ -2,6 +2,10 @@ import axios from '@/lib/axios';
 import { GenreEntity } from '@/types/genre';
 
 export const genreApi = {
+  /**
+   * 모든 장르 목록을 가져옵니다.
+   * @returns 전체 장르 목록
+   */
   getAllGenres: () => {
     return axios.get<GenreEntity[]>('/genre');
   },
