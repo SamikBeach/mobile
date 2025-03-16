693989cf16f644ae6f40b2521dd39e61f948936a
bong <jungcome7@gmail.com>
2025-03-16 12:21:25 +0900

    chore: 주석 추가

--- src/apis/search.ts
+++ src/apis/search.ts
@@ -8,6 +8,11 @@ interface SearchResponse {
 }
 
 export const searchApi = {
+  /**
+   * 도서와 작가를 검색합니다.
+   * @param keyword - 검색어
+   * @returns 검색된 도서와 작가 목록
+   */
   search: (keyword: string) =>
     axios.get<SearchResponse>('/search', {
       params: {
